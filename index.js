0284612df34a422dae5f7fb392b47e2a8ca89c2d
DeveloperNomis <nemo.basil12@gmail.com>
2020-05-21 19:34:10 +0200

    Running Server

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
 
 
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/chat', (req, res) => {
+  res.render('pages/chat');
+});
+
+
+app.listen(port, () => {
+
+  console.log(`Server is running on Port: ${port}`);
+
+});
